122bac891abbc0e6f6bc3c948f75d6249c23955a
Volodymyr Yaremchak <vovkahorror@gmail.com>
2022-07-11 18:28:55 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strCopy = str;
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strCopy.includes(bracketsConfig[j].join(""))) {
+        strCopy =
+          strCopy.slice(0, strCopy.indexOf(bracketsConfig[j].join(""))) +
+          strCopy.slice(strCopy.indexOf(bracketsConfig[j].join("")) + 2);
+      }
+    }
+  }
+
+  if (strCopy.length === 0) {
+    return true;
+  }
+
+  return false;
+};
